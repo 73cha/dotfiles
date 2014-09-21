5ffd20765120bfa6e9c82dd85654050f4eea70cc
Katsuhisa <katsuhisa.namizaki+github@gmail.com>
2014-09-22 08:00:55 +0900

    ターミナルのカラースキームを追加

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-dotfiles=(.railsrc .gemrc .vimrc .tmux.conf .zshrc)
+dotfiles=(.railsrc .gemrc .vimrc .tmux.conf .zshrc tmux_status-bar_shell/*)
 
 for i in ${dotfiles[@]}; do
   ln -sf ~/dotfiles/$i ~/$i
-  echo "\033[36m${i}のシンボリックリンクを${HOME}/dotfilesへ作りました。\033[0m"
+  echo "\033[36m${HOME}にdotfiles/${i}のシンボリックリンクを作りました。\033[0m"
 done
