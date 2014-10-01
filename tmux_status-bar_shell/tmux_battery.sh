18114a378851f7195b743339968066498fc84634
Katsuhisa <katsuhisa.namizaki+github@gmail.com>
2014-10-02 04:42:35 +0900

    tmuxステータスラインのカスタマイズファイル調整

--- tmux_status-bar_shell/tmux_battery.sh
+++ tmux_status-bar_shell/tmux_battery.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
+#!/usr/local/bin/zsh
 # /usr/bin/pmset -g ps | awk '{ if (NR == 2) print "Batteries:" $2 " Status:" $3 " " $4 " " $5; }' | sed "s/;//g"
 /usr/bin/pmset -g ps | awk '{ if (NR == 2) print " ♥ :" $2 " (" $3 ") "; }' | sed "s/;//g"
--- tmux_status-bar_shell/tmux_memory.sh
+++ tmux_status-bar_shell/tmux_memory.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
-top -e -l 1 | grep "PhysMem:" | sed -e 's/PhysMem: //' -e 's/M//' | awk '{ convert_g=substr($1/1000, 0, 4) }END{ print "メモリ: " sprintf("%sG",convert_g) }'
-
+#!/usr/local/bin/zsh
+# top -e -l 1 | grep "PhysMem:" | sed -e 's/PhysMem: //' -e 's/M//' | awk '{ convert_g=substr($1/1000, 0, 4) }END{ print "メモリ: " sprintf("%sG",convert_g) }'
+top -e -l 1 | grep "PhysMem:" | sed -e 's/PhysMem: //' | awk ' { print "メモリ使用量: " $1 }'
